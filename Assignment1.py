d883b4e316397d13ba78379a18dce2fd631ba2db
Sakshamsf <91115477+Sakshamsf@users.noreply.github.com>
2021-09-21 12:53:29 +0530

    Delete Assignment1.py

--- Assignment1.py
+++
@@ -1,7 +0,0 @@
-#Calculating Simple Interest
-
-principle = int(input("Enter the principal amount "))
-rate = float(input(" Enter the rate of interest  "))
-time = int(input( " Enter the number of years "))
-simple_interest = (principle * rate * time)/100
-print("The simple interest is ",simple_interest)
